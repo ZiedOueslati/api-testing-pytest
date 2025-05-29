eddcf232aef0a2262e33d8ead5ee4d6213299625
ZiedOueslati <oueslati.zied4@gmail.com>
2025-05-29 11:35:42 +0100

    Organize tests into folder and add markers

--- tests/test_posts_api.py
+++ tests/test_posts_api.py
@@ -1,15 +1,16 @@
 import requests
+import pytest
 
-BASE_URL = "https://jsonplaceholder.typicode.com"
-
-def test_get_all_posts():
-    response = requests.get(f"{BASE_URL}/posts")
+@pytest.mark.smoke
+def test_get_all_posts(base_url):
+    response = requests.get(f"{base_url}/posts")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
     assert len(response.json()) == 100  # JSONPlaceholder returns 100 posts
 
-def test_get_single_post():
-    response = requests.get(f"{BASE_URL}/posts/1")
+@pytest.mark.regression
+def test_get_single_post(base_url):
+    response = requests.get(f"{base_url}/posts/1")
     assert response.status_code == 200
     data = response.json()
     assert data["id"] == 1
